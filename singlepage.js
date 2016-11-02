405c829fe0a001009ada39c963cf8a0051a264be
Andrew-William-Smith <andrewwilliamsmith@yahoo.com>
2016-11-02 10:09:08 -0500

    Prevented blank input on single-page generator
    Deleting all text would previously render a field invisible; this is now
    impossible.

--- singlepage.js
+++ singlepage.js
@@ -1,8 +1,21 @@
+function commitNewText(element) {
+    if (element.val() == '') {
+        element.val(element.attr('default'));
+        element.autoGrowInput({minWidth: 20, maxWidth: 1000, comfortZone: 0});
+    }
+}
+
 $(document).ready(function() {
     $(':text').autoGrowInput({minWidth: 20, maxWidth: 1000, comfortZone: 0});
 
     $('input[type=file]').change(function(evt) {
         $('#' + $(this).attr('target')).attr('src',
                                              URL.createObjectURL(evt.target.files[0]));
+    });
+
+    $(':text').keypress(function(evt) {
+        if (evt.which == 13) commitNewText($(this));  // Enter
+    }).focusout(function() {
+        commitNewText($(this));
     });
 });
